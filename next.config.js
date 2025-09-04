90c4c6bb6ca6f31fea36df49f13830f921c729eb
Mahmoud Hisham <mahmoudhisham11@gmail.com>
2025-09-04 19:00:12 +0300

    Add new files and updates

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  i18n: {
-    locales: ['ar', 'en'],   // اللغات المدعومة
-    defaultLocale: 'en',     // الإنجليزية افتراضي
-  },
 }
 
 module.exports = nextConfig
