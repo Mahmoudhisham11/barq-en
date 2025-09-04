714671b0f0599feaa53be64ad78aa2f2e6e90418
Mahmoud Hisham <mahmoudhisham11@gmail.com>
2025-09-04 20:47:32 +0300

    Add new files and updates

--- app/page.jsx
+++ app/page.jsx
@@ -1,16 +1,18 @@
 'use client';
 
 import { useSearchParams } from "next/navigation";
-import Hero from "@/components/Hero/Hero";
-import About from "@/components/About/About";
-import Services from "@/components/Services/Services";
-import Contact from "@/components/Contact/Contact";
-import Footer from "@/components/Footer/Footer";
+import dynamic from "next/dynamic";
 import ar from "../locales/ar.json";
 import en from "../locales/en.json";
 
+// استيراد المكونات بطريقة dynamic لمنع SSR
+const Hero = dynamic(() => import("@/components/Hero/Hero"), { ssr: false });
+const About = dynamic(() => import("@/components/About/About"), { ssr: false });
+const Services = dynamic(() => import("@/components/Services/Services"), { ssr: false });
+const Contact = dynamic(() => import("@/components/Contact/Contact"), { ssr: false });
+const Footer = dynamic(() => import("@/components/Footer/Footer"), { ssr: false });
+
 export default function HomePage() {
-  // نستخدم hook للـ client side فقط
   const searchParams = useSearchParams();
   const locale = searchParams?.get("locale") || "en"; // تحديد اللغة من الـ URL
   const t = locale === "ar" ? ar : en; // اختيار اللغة المناسبة
